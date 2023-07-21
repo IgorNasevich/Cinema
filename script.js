1922053d475090f7ccce2f83cef405a079f45f92
IgorNasevich <igor.nasevich007@gmail.com>
2023-07-21 21:21:49 +0300

    fix: rebuild js media query algorithm

--- script.js
+++ script.js
@@ -1,59 +1,106 @@
 let card5 = document.getElementById("5-card");
 let card6 = document.getElementById("6-card");
 
+const PCScreen = 0;
+const TabletScreen = 1;
+const MobileScreen = 2;
+let currentScreen = -1;
+
 window.onresize = function(){
+    let newScreen;
     if(document.documentElement.clientWidth <= 1180){
-        //change image
-        document.getElementById("main-image").setAttribute("src", "/assets/images/middle-size-main-image.png");
-        //replace button
-            let button1 = document.getElementsByClassName("buy-button")[0];
-            document.getElementsByClassName("buy-button")[0].remove();
-            document.getElementsByClassName("review")[0].after(button1);
-        //change film name
-        document.getElementsByClassName("film-name")[0].innerText = "Операция «Фортуна»: Искусство побеждать";
-        //delete 2 last cards;
-        if(document.getElementById("5-card")!==false){
-            document.getElementById("5-card").remove();
-            document.getElementById("6-card").remove();
-        }
-        //change card pictures
-        document.getElementById("1-card").firstElementChild.setAttribute("src", "/assets/images/1-card-tablet.png");
-        document.getElementById("2-card").firstElementChild.setAttribute("src", "/assets/images/2-card-tablet.png");
-        document.getElementById("3-card").firstElementChild.setAttribute("src", "/assets/images/3-card-tablet.png");
-        document.getElementById("4-card").firstElementChild.setAttribute("src", "/assets/images/4-card-tablet.png");
-        //change cards names
-        document.getElementsByClassName("film-info__name")[0].innerText = "Аватар: Путь воды";
-        document.getElementsByClassName("film-info__name")[1].innerText = "Мой домашний крокодил";
-        document.getElementsByClassName("film-info__name")[2].innerText = "Амстердам";
-        document.getElementsByClassName("film-info__name")[3].innerText = "Знакомство родителей";
-
-
-
+       newScreen = TabletScreen;
     }
-    else {
-        //return image
-        document.getElementById("main-image").setAttribute("src", "/assets/images/main-image.png" );
-        //return button
-        let button1 = document.getElementsByClassName("buy-button")[0];
-        document.getElementsByClassName("buy-button")[0].remove();
-        document.getElementsByClassName("current-film-image")[0].after(button1);
-        //return film name
-        document.getElementsByClassName("film-name")[0].innerText = "Элвис";
-        //return 2 last cards
-        document.getElementById("4-card").after(card5);
-        document.getElementById("5-card").after(card6);
-        //return card pictures
-        document.getElementById("1-card").firstElementChild.setAttribute("src", "/assets/images/1-card.png");
-        document.getElementById("2-card").firstElementChild.setAttribute("src", "/assets/images/2-card.png");
-        document.getElementById("3-card").firstElementChild.setAttribute("src", "/assets/images/3-card.png");
-        document.getElementById("4-card").firstElementChild.setAttribute("src", "/assets/images/4-card.png");
-        //return cards names
-        //change cards names
-        document.getElementsByClassName("film-info__name")[0].innerText = "Меган";
-        document.getElementsByClassName("film-info__name")[1].innerText = "Быстрее пули";
-        document.getElementsByClassName("film-info__name")[2].innerText = "Земун";
-        document.getElementsByClassName("film-info__name")[3].innerText = "Элвис";
+    else if (document.documentElement.clientWidth <= 834){
+        newScreen = MobileScreen;
+    }
+    else{
+        newScreen = PCScreen;
+    }
+
+    if(currentScreen !== newScreen){
+        currentScreen = newScreen;
+        if(currentScreen === PCScreen){
+            setPCLayout();
+        }
+        if(currentScreen === TabletScreen){
+            setTabletLayout();
+        }
+        if(currentScreen === MobileScreen){
+            setMobileLayout();
+        }
     }
 }
 
+function setTabletLayout () {
+    //change image
+    document.getElementById("main-image").setAttribute("src", "/assets/images/middle-size-main-image.png");
+    //replace button
+    let button1 = document.getElementsByClassName("buy-button")[0];
+    document.getElementsByClassName("buy-button")[0].remove();
+    document.getElementsByClassName("review")[0].after(button1);
+    //change film name
+    document.getElementsByClassName("film-name")[0].innerText = "Операция «Фортуна»: Искусство побеждать";
+    //delete 2 last cards;
+    document.getElementById("5-card").remove();
+    document.getElementById("6-card").remove();
+    //change card pictures
+    document.getElementById("1-card").firstElementChild.setAttribute("src", "/assets/images/1-card-tablet.png");
+    document.getElementById("2-card").firstElementChild.setAttribute("src", "/assets/images/2-card-tablet.png");
+    document.getElementById("3-card").firstElementChild.setAttribute("src", "/assets/images/3-card-tablet.png");
+    document.getElementById("4-card").firstElementChild.setAttribute("src", "/assets/images/4-card-tablet.png");
+    //change cards names
+    document.getElementsByClassName("film-info__name")[0].innerText = "Аватар: Путь воды";
+    document.getElementsByClassName("film-info__name")[1].innerText = "Мой домашний крокодил";
+    document.getElementsByClassName("film-info__name")[2].innerText = "Амстердам";
+    document.getElementsByClassName("film-info__name")[3].innerText = "Знакомство родителей";
+    }
 
+    function setPCLayout () {
+    //return image
+    document.getElementById("main-image").setAttribute("src", "/assets/images/main-image.png" );
+    //return button
+    let button1 = document.getElementsByClassName("buy-button")[0];
+    document.getElementsByClassName("buy-button")[0].remove();
+    document.getElementsByClassName("current-film-image")[0].after(button1);
+    //return film name
+    document.getElementsByClassName("film-name")[0].innerText = "Элвис";
+    //return 2 last cards
+    document.getElementById("4-card").after(card5);
+    document.getElementById("5-card").after(card6);
+    //return card pictures
+    document.getElementById("1-card").firstElementChild.setAttribute("src", "/assets/images/1-card.png");
+    document.getElementById("2-card").firstElementChild.setAttribute("src", "/assets/images/2-card.png");
+    document.getElementById("3-card").firstElementChild.setAttribute("src", "/assets/images/3-card.png");
+    document.getElementById("4-card").firstElementChild.setAttribute("src", "/assets/images/4-card.png");
+    //return cards names
+    document.getElementsByClassName("film-info__name")[0].innerText = "Меган";
+    document.getElementsByClassName("film-info__name")[1].innerText = "Быстрее пули";
+    document.getElementsByClassName("film-info__name")[2].innerText = "Земун";
+    document.getElementsByClassName("film-info__name")[3].innerText = "Элвис";
+}
+
+
+function setMobileLayout(){
+
+}
+
+// window.onresize = function(){
+//     let newScreen;
+//     if(document.documentElement.clientWidth <= 1180){
+//        newScreen = TabletScreen;
+//     }
+//     else {
+//         newScreen = PCScreen;
+//     }
+
+//     if(currentScreen !== newScreen){
+//         currentScreen = newScreen;
+//         if(currentScreen === PCScreen){
+//             setPCLayout();
+//         }
+//         if(currentScreen === TabletScreen){
+//             setTabletLayout();
+//         }
+//     }
+// }
